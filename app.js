c81703dae93637519daa4d4af6da5a515a018eda
parasj93 <parasjain93@gmail.com>
2018-05-27 14:36:38 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const hbs = require('hbs');
+var app = express();
+
+app.use((req,res,next)=>{
+    next();
+})
+
+hbs.registerPartials(__dirname + '/views/partials')
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+})
+
+
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
+//handlers bar to handle the request......
+app.set('view-engine', 'hbs');
+
+app.get('/', (req, res) => {
+    //res.send('hello World');
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        message: 'Welcome to Node App'
+    })
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+
+
+app.use(express.static(__dirname + '/public'));
+app.listen(3000, () => {
+    console.log('server started at port 3000');
+});
